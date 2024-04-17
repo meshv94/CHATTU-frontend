1b4c116c04cc0b2529914c64d1c62f4a55970bdb
meshv office <meshvpatel.ics@gmail.com>
2024-04-17 15:47:34 +0530

    message bug solved

--- src/components/messageComponent/Message.jsx
+++ src/components/messageComponent/Message.jsx
@@ -6,15 +6,17 @@ export const Message = ({ message }) => {
   // console.log("andar aya kya", message);
   const user = JSON.parse(localStorage.getItem("user")) || null;
   const { selectedConversation } = useConversation();
+  // console.log('selected conversion' , selectedConversation)
   const fromMe = message.senderId === user._id;
   const chatClass = fromMe ? "chat-end" : "chat-start";
   const avatar = fromMe ? user.avatar : selectedConversation?.avatar;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   const formatedTime = extractTime(message.createdAt);
 
+  if(message.senderId === selectedConversation._id || message.senderId === user._id){
   return (
     <>
-      <div className={`chat ${chatClass}`}>
+        <div className={`chat ${chatClass}`}>
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img alt="Tailwind CSS chat bubble component" src={avatar} />
@@ -26,5 +28,6 @@ export const Message = ({ message }) => {
         </div>
       </div>
     </>
-  );
+  );}
+
 };
