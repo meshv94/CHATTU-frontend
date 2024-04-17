64071ed25be5bb22f8fc5f90785580d2b71b57d6
meshv office <meshvpatel.ics@gmail.com>
2024-04-17 13:31:06 +0530

    message not send issue solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,11 @@ export const Home = () => {
   const user = JSON.parse(localStorage.getItem("user")) || null;
 
   useEffect(() => {
-    connectSocket(user)
+    // connectSocket(user)
   }, []);
   
   return (
-    <>
-        {/* <header className="w-full h-15 bg-base-100 flex"> */}
-          {/* <h2 className="text-xl italic font-bold text-blue-100 self-center ml-3">ChatApp</h2> */}
-        {/* </header> */}
-
-      
+    <>     
       <div className="flex flex-col lg:flex-row justify-between w-full h-full">
           <SliderBar />
           <MessageComponent />
